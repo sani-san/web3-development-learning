0497414bf9aa2384a9c4141389cfe09e7c6d2000
sani-san <anthonypisani_dev@outlook.com>
2024-08-03 13:39:54 -0400

    moved contracts into section folder + commented out deployed contracts in migrations folder

--- migrations/1_deploy_contract.js
+++ migrations/1_deploy_contract.js
@@ -1,17 +1,25 @@
-const Types = artifacts.require("Types");
-const WrapAround = artifacts.require("WrapAround");
-const Strings = artifacts.require("Strings");
-const Addresses = artifacts.require("Addresses");
-const MsgSender = artifacts.require("MsgSender");
-const ViewPure = artifacts.require("ViewPure");
-const Constructor = artifacts.require("Constructor");
+// // section 3
+
+// const Types = artifacts.require("Types");
+// const WrapAround = artifacts.require("WrapAround");
+// const Strings = artifacts.require("Strings");
+// const Addresses = artifacts.require("Addresses");
+// const MsgSender = artifacts.require("MsgSender");
+// const ViewPure = artifacts.require("ViewPure");
+// const Constructor = artifacts.require("Constructor");
+
+// module.exports = function (deployer) {
+//   deployer.deploy(Types);
+//   deployer.deploy(WrapAround);
+//   deployer.deploy(Strings);
+//   deployer.deploy(Addresses);
+//   deployer.deploy(MsgSender);
+//   deployer.deploy(ViewPure);
+//   deployer.deploy(Constructor);
+// };
+
+const x = artifacts.require('');
 
 module.exports = function (deployer) {
-  deployer.deploy(Types);
-  deployer.deploy(WrapAround);
-  deployer.deploy(Strings);
-  deployer.deploy(Addresses);
-  deployer.deploy(MsgSender);
-  deployer.deploy(ViewPure);
-  deployer.deploy(Constructor);
-};
+
+}
