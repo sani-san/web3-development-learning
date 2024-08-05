6f8e9ba611638828e51fc7f6ae0638539fe2be96
sani-san <anthonypisani_dev@outlook.com>
2024-08-05 10:24:26 -0400

    interaction with smart contract using web3

--- migrations/1_deploy_contract.js
+++ migrations/1_deploy_contract.js
@@ -1,32 +1,32 @@
 // * SECTION 5
 
-const Mapping = artifacts.require("Mapping");
-const PracticalMapping = artifacts.require("PracticalMapping");
-const Struct = artifacts.require("Struct");
-const MappingStruct = artifacts.require("MappingStructs");
-const ExceptionRequire = artifacts.require("ExceptionRequire");
-const TryCatch = artifacts.require("TryCatch");
-const Sender = artifacts.require("Sender");
-const ReceiverNoAction = artifacts.require("ReceiverNoAction");
-const ReceiverAction = artifacts.require("ReceiverAction");
-const ContractOne = artifacts.require("ContractOne");
-const ContractTwo = artifacts.require("ContractTwo");
-const SmartWallet = artifacts.require("SmartWallet");
+// const Mapping = artifacts.require("Mapping");
+// const PracticalMapping = artifacts.require("PracticalMapping");
+// const Struct = artifacts.require("Struct");
+// const MappingStruct = artifacts.require("MappingStructs");
+// const ExceptionRequire = artifacts.require("ExceptionRequire");
+// const TryCatch = artifacts.require("TryCatch");
+// const Sender = artifacts.require("Sender");
+// const ReceiverNoAction = artifacts.require("ReceiverNoAction");
+// const ReceiverAction = artifacts.require("ReceiverAction");
+// const ContractOne = artifacts.require("ContractOne");
+// const ContractTwo = artifacts.require("ContractTwo");
+// const SmartWallet = artifacts.require("SmartWallet");
 
-module.exports = async function (deployer) {
-  await deployer.deploy(Mapping);
-  await deployer.deploy(PracticalMapping);
-  await deployer.deploy(Struct);
-  await deployer.deploy(MappingStruct);
-  await deployer.deploy(ExceptionRequire);
-  await deployer.deploy(TryCatch);
-  await deployer.deploy(Sender);
-  await deployer.deploy(ReceiverAction);
-  await deployer.deploy(ReceiverNoAction);
-  await deployer.deploy(ContractOne);
-  await deployer.deploy(ContractTwo);
-  await deployer.deploy(SmartWallet);
-}
+// module.exports = async function (deployer) {
+//   await deployer.deploy(Mapping);
+  // await deployer.deploy(PracticalMapping);
+  // await deployer.deploy(Struct);
+  // await deployer.deploy(MappingStruct);
+  // await deployer.deploy(ExceptionRequire);
+  // await deployer.deploy(TryCatch);
+  // await deployer.deploy(Sender);
+  // await deployer.deploy(ReceiverAction);
+  // await deployer.deploy(ReceiverNoAction);
+  // await deployer.deploy(ContractOne);
+  // await deployer.deploy(ContractTwo);
+  // await deployer.deploy(SmartWallet);
+// };
 
 // * SECTION 4 
 
@@ -42,20 +42,20 @@ module.exports = async function (deployer) {
 
 // * SECTION 3 
 
-// const Types = artifacts.require("Types");
-// const WrapAround = artifacts.require("WrapAround");
-// const Strings = artifacts.require("Strings");
-// const Addresses = artifacts.require("Addresses");
-// const MsgSender = artifacts.require("MsgSender");
-// const ViewPure = artifacts.require("ViewPure");
-// const Constructor = artifacts.require("Constructor");
+const Types = artifacts.require("Types");
+const WrapAround = artifacts.require("WrapAround");
+const Strings = artifacts.require("Strings");
+const Addresses = artifacts.require("Addresses");
+const MsgSender = artifacts.require("MsgSender");
+const ViewPure = artifacts.require("ViewPure");
+const Constructor = artifacts.require("Constructor");
 
-// module.exports = function (deployer) {
-//   deployer.deploy(Types);
-//   deployer.deploy(WrapAround);
-//   deployer.deploy(Strings);
-//   deployer.deploy(Addresses);
-//   deployer.deploy(MsgSender);
-//   deployer.deploy(ViewPure);
-//   deployer.deploy(Constructor);
-// };
+module.exports = function (deployer) {
+  deployer.deploy(Types);
+  // deployer.deploy(WrapAround);
+  // deployer.deploy(Strings);
+  // deployer.deploy(Addresses);
+  // deployer.deploy(MsgSender);
+  // deployer.deploy(ViewPure);
+  // deployer.deploy(Constructor);
+};
